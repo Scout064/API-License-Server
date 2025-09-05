1122da6562967aca5c3939aa12b4cf361ad19cbb
Scout064 <78973583+Scout064@users.noreply.github.com>
2025-09-05 13:44:28 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app import models, schemas
+from app import models
 
 router = APIRouter()
 
