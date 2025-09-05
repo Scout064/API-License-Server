d935264bfd04e0946bced557f9bf3e4f9c8b25e6
Scout064 <78973583+Scout064@users.noreply.github.com>
2025-09-05 12:23:12 +0200

    Update wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -9,3 +9,6 @@ sys.path.insert(0, "/var/www/license-server")
 # activate virtualenv
 # Add virtual environment site-packages
 site.addsitedir('/var/www/license-server/lic/lib/python3.11/site-packages')
+
+# Import your Flask app
+from app import app as application
