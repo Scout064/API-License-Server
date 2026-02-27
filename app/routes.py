78912f21936289827492212782aa2114630e0161
Scout064 <78973583+Scout064@users.noreply.github.com>
2026-02-27 13:32:13 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Path
 from sqlalchemy.orm import Session
-from app.models import ClientBase, Client, LicenseBase, License, hash_license_key
+from app.models import ClientBase, Client, LicenseBase, License, hash_license_key, ClientORM, LicenseORM
 from app.database import get_db
 from app.auth import require_role
 from fastapi_limiter.depends import RateLimiter
