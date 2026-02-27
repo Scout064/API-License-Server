215be888ec9a1ee39864997f88dd81ca9913c9c5
Scout064 <78973583+Scout064@users.noreply.github.com>
2026-02-27 13:18:32 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,9 +6,10 @@ from slowapi import Limiter
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from app.database import get_db
-from app.models import LicenseORM, ClientORM, hash_license
 from app import schemas
 from app.auth import require_role
+from app.models import LicenseORM, ClientORM
+from app.auth import hash_license_key
 
 # Rate Limiter (10 per minute per IP)
 limiter = Limiter(key_func=get_remote_address)
