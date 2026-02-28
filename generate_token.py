ec08c5ffe108375d13ea0b61e82839f70c4a3888
Scout064 <78973583+Scout064@users.noreply.github.com>
2026-02-28 21:37:40 +0100

    Update generate_token.py

--- generate_token.py
+++ generate_token.py
@@ -3,6 +3,15 @@ import os
 import jwt
 from datetime import datetime, timedelta
 
+# Attempt to load the .env file if it exists in the current directory
+try:
+    with open("/var/www/licenseapi/.env", "r") as f:
+        for line in f:
+            if line.startswith("JWT_SECRET="):
+                os.environ["JWT_SECRET"] = line.strip().split("=", 1)[1]
+except FileNotFoundError:
+    pass
+
 def main():
     print("=== API License Server - JWT Generator ===")
     
