80b7adaf0febfcec685025da65fbe11c1ad94b87
Scout064 <78973583+Scout064@users.noreply.github.com>
2026-03-01 20:18:07 +0100

    Update main.py
    Updated Version to v2.1.3b-beta.3

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="License Server API",
     description="API for license generation, validation, and revocation",
-    version="v2.1.2b-beta.2",
+    version="v2.1.3b-beta.3",
     lifespan=lifespan
 )
 
